fbfcc4e4330a16cfd6fad6dc4fb7ec86ced2bcba
karpovkottt <karpov.kottt@gmail.com>
2023-02-14 12:09:18 +0700

    feat: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+       this.min = min;
+       this.max = max;      
     }
 
     guess() {
-
+       return this.middle = Math.round((this.min + this.max) / 2);
     }
 
-    lower() {
-
+    lower() {  
+       return this.max = this.middle;
     }
 
     greater() {
-
+       return this.min = this.middle;
     }
 }
 
